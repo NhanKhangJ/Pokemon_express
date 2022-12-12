b219bcac826545ee698107f539d44f50c3238b13
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2022-12-12 11:24:16 -0700

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const app = express();
 const port = 3000;
 const pokemon = require('./models/pokemon.js')
 
+
+
 app.get('/', (req,res)=>{
     res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req,res)=>{
-    
-    res.send(pokemon);
+      res.render('Index')
 })
 
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 app.listen(port, ()=>{
     console.log('Server running on'+ port)
---
+++ views/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+  };
+const Index = () => {
+  return (
+    <div style={myStyle}>My First React Component!</div>
+  )
+}
+
+export default Index
